2a8bdc4236e399a98a8e1186165778d3fb612ccc
Rakibul Islam <rakibul.islam.dev@gmail.com>
2022-10-29 15:22:40 +0600

    Removed extra work for csv type

--- index.js
+++ index.js
@@ -8,73 +8,71 @@ const phantomJsCloud = require("phantomjscloud");
 let apiKey = undefined;
 let browser = new phantomJsCloud.BrowserApi(apiKey);
 
+//create the csv writer
+const csvWriter = createCsvWriter({
+  path: "data.csv",
+  header: [
+    { id: "id", title: "ID" },
+    { id: "airbnb_property_id", title: "Aribnb Property ID" },
+    { id: "homeaway_property_id", title: "Homeaway Property ID" },
+    { id: "m_homeaway_property_id", title: "M Homeaway Property ID" },
+    { id: "title", title: "Title" },
+    { id: "room_type", title: "Room Type" },
+    { id: "property_type", title: "Property Type" },
+    { id: "adr", title: "Avg. Daily Rate" },
+    { id: "occ", title: "Occupancy" },
+    { id: "revenue", title: "Revenue" },
+    { id: "reviews", title: "Total Reviews" },
+    { id: "rating", title: "Rating" },
+    { id: "bedrooms", title: "Bedrooms" },
+    { id: "bathrooms", title: "Bathrooms" },
+    { id: "accommodates", title: "Accommodates" },
+    { id: "latitude", title: "Latitude" },
+    { id: "longitude", title: "Longitude" },
+    { id: "days_available", title: "Days Available" },
+    { id: "img_cover", title: "Image Cover" },
+    { id: "link", title: "Link" },
+    { id: "regions", title: "Regions" },
+  ],
+});
+
 app.use(express.static("public"));
 
-async function saveData(data) {
-  //create json file
-  await fs.writeFile(
-    "data.json",
-    JSON.stringify(
-      data.properties.map((item) => {
-        let newItem = {
-          id: item.id,
-          airbnb_property_id: item.airbnb_property_id,
-          homeaway_property_id: item.homeaway_property_id,
-          m_homeaway_property_id: item.m_homeaway_property_id,
-          title: item.title,
-          room_type: item.room_type,
-          property_type: item.property_type,
-          adr: item.adr,
-          occ: item.occ,
-          revenue: item.revenue,
-          reviews: item.reviews,
-          rating: item.rating,
-          bedrooms: item.bedrooms,
-          bathrooms: item.bathrooms,
-          accommodates: item.accommodates,
-          latitude: item.latitude,
-          longitude: item.longitude,
-          days_available: item.days_available,
-          img_cover: item.img_cover,
-        };
-        newItem.link = `https://airbnb.com/rooms/${item.airbnb_property_id}`;
-        newItem.regions = item.regions.zipcode_ids;
-        return newItem;
-      })
-    )
-  );
-
-  //create csv file
-  const csvWriter = createCsvWriter({
-    path: "data.csv",
-    header: [
-      { id: "id", title: "ID" },
-      { id: "airbnb_property_id", title: "Aribnb Property ID" },
-      { id: "homeaway_property_id", title: "Homeaway Property ID" },
-      { id: "m_homeaway_property_id", title: "M Homeaway Property ID" },
-      { id: "title", title: "Title" },
-      { id: "room_type", title: "Room Type" },
-      { id: "property_type", title: "Property Type" },
-      { id: "adr", title: "Avg. Daily Rate" },
-      { id: "occ", title: "Occupancy" },
-      { id: "revenue", title: "Revenue" },
-      { id: "reviews", title: "Total Reviews" },
-      { id: "rating", title: "Rating" },
-      { id: "bedrooms", title: "Bedrooms" },
-      { id: "bathrooms", title: "Bathrooms" },
-      { id: "accommodates", title: "Accommodates" },
-      { id: "latitude", title: "Latitude" },
-      { id: "longitude", title: "Longitude" },
-      { id: "days_available", title: "Days Available" },
-      { id: "img_cover", title: "Image Cover" },
-      { id: "link", title: "Link" },
-      { id: "regions", title: "Regions" },
-    ],
+async function saveData(data, fileType) {
+  let properties = data.properties.map((item) => {
+    let newItem = {
+      id: item.id,
+      airbnb_property_id: item.airbnb_property_id,
+      homeaway_property_id: item.homeaway_property_id,
+      m_homeaway_property_id: item.m_homeaway_property_id,
+      title: item.title,
+      room_type: item.room_type,
+      property_type: item.property_type,
+      adr: item.adr,
+      occ: item.occ,
+      revenue: item.revenue,
+      reviews: item.reviews,
+      rating: item.rating,
+      bedrooms: item.bedrooms,
+      bathrooms: item.bathrooms,
+      accommodates: item.accommodates,
+      latitude: item.latitude,
+      longitude: item.longitude,
+      days_available: item.days_available,
+      img_cover: item.img_cover,
+    };
+    newItem.link = `https://airbnb.com/rooms/${item.airbnb_property_id}`;
+    newItem.regions = item.regions.zipcode_ids;
+    return newItem;
   });
 
-  const records = require("./data.json");
-
-  await csvWriter.writeRecords(records);
+  if (fileType === "json") {
+    //create json file
+    await fs.writeFile("data.json", JSON.stringify(properties));
+  } else if (fileType === "csv") {
+    //create csv file
+    await csvWriter.writeRecords(properties);
+  }
 }
 
 app.get("/get-data", async (req, res) => {
@@ -112,7 +110,7 @@ app.get("/get-data", async (req, res) => {
       const response = await fetch(targetURL);
       const data = await response.json();
 
-      await saveData(data);
+      await saveData(data, fileType);
 
       if (fileType === "csv") {
         return res.download("data.csv");
